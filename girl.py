a84c6b3f265fd06c897c293acd537bb2da0c232a
KiMinje <under201@gmail.com>
2025-11-10 13:08:00 +0900

    girl

---
+++ girl.py
@@ -0,0 +1,29 @@
+from pico2d import load_image, get_time, load_font, draw_rectangle, close_canvas
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
+
+import game_world
+import game_framework
+
+from state_machine import StateMachine
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+class Idle:
+
+class Run:
+
+class Girl:
+
