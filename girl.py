16d567f9496d365da5028d0a8b57f910c6c15334
KiMinje <under201@gmail.com>
2025-11-10 16:49:04 +0900

    speed

--- girl.py
+++ girl.py
@@ -22,6 +22,22 @@ def left_down(e):
 def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+WALK_SPEED_KMPH = 20.0  # Km / Hour
+WALK_SPEED_MPM = (WALK_SPEED_KMPH * 1000.0 / 60.0)
+WALK_SPEED_MPS = (WALK_SPEED_MPM / 60.0)
+WALK_SPEED_PPS = (WALK_SPEED_MPS * PIXEL_PER_METER)
+
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 40.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
 class ResourceManager:
     _images = {}\
 
@@ -54,7 +70,7 @@ class Idle:
         self.girl.dir = 0
 
     def do(self):
-        self.girl.frame = (self.girl.frame + 1) % 8
+        self.girl.frame = (self.girl.frame + 1) % 12
 
     def exit(self):
         pass
