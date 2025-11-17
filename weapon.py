a02b062e09487254826df8a1a43158fdbb9419c0
KiMinje <under201@gmail.com>
2025-11-18 01:48:59 +0900

    틀

--- weapon.py
+++ weapon.py
@@ -2,6 +2,25 @@ import os
 from pico2d import load_image
 
 class Weapon:
+    def __init__():
+
+    def is_visible_in():
+
+    def _compute_frame():
+
+    def draw():
 
 
 class WeaponManager:
+
+    def __init__():
+
+    def add_weapon():
+
+    def equip():
+
+    def unequip():
+
+    def ger_equiped():
+
+    def draw():
