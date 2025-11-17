3884eb1d6cc9995003ad3081ef3fc425621c91f9
KiMinje <under201@gmail.com>
2025-11-18 04:07:37 +0900

    idle 무기

--- weapons_config.py
+++ weapons_config.py
@@ -1,7 +1,9 @@
 import os
 from weapon import Weapon, WeaponManager
 
-DEFAULT_RUN_OFFSET = (-50, 3)
+RUN_OFFSET = (-50, 3)
+
+IDLE_OFFSET = (-50, 3)
 
 def create_default_weapon_manager():
     manager = WeaponManager()
@@ -9,7 +11,8 @@ def create_default_weapon_manager():
 
     pink_sword = Weapon(
         sheets = {
-            'run': ('run_pink.png', 12, DEFAULT_RUN_OFFSET, None),
+            'run': ('run_pink.png', 12, RUN_OFFSET, None),
+            'idle': ('idle_pink.png', 1, IDLE_OFFSET, None),
         },
         default_state = 'run'
     )
