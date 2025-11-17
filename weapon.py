46389080ef927d265f02f69db096cd743138e24d
KiMinje <under201@gmail.com>
2025-11-18 01:53:15 +0900

    무기 관리 매니저

--- weapon.py
+++ weapon.py
@@ -20,10 +20,21 @@ class WeaponManager:
     def add_weapon(self, name, weapon: Weapon):
         self.weapons[name] = weapon
 
-    def equip():
+    def equip(self, name):
+        if name in self.weapons:
+            self.equipped = name
+        else:
+            print(f'[WeaponManager] equip 실패: {name} 없음')
 
-    def unequip():
+    def unequip(self):
+        self.equipped = None
 
-    def ger_equiped():
+    def ger_equiped(self):
+        return self.weapons.get(self.equipped)
 
-    def draw():
+    def draw(elf, cx, cy, face_dir, char_frame_index=0, char_frame_count=1, state_name=None):
+        w = self.get_equipped()
+        if not w:
+            return
+        w.draw(cx, cy, face_dir, char_frame_index=char_frame_index, char_frame_count=char_frame_count,
+               state_name=state_name)
