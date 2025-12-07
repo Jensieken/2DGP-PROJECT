1a2071addedb0c9fe091c30333f2ac6e4c196815
KiMinje <under201@gmail.com>
2025-12-08 01:27:45 +0900

    공중이면 좌표이동

--- girl.py
+++ girl.py
@@ -400,6 +400,9 @@ class Normal_Attack:
             elif event.key == SDLK_LEFT:
                 self.girl.face_dir = -1
 
+        if getattr(self.girl, 'ground_y', self.girl.y) <= self.girl.y + 1:
+            self.girl.dir = 0
+
         self.girl.dir = 0
         self.girl.frame = 0.0
         self.timer = 0.0
